fd72e124b5c428aced8f844dc8089e7cd4d3adb3
francisco.gallegos.luque <franciscogallegosluque@gmail.com>
2025-02-07 21:09:02 -0300

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Francisco Manuel Gallegos Luque on 07/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CupcakeCornerTests/CupcakeCornerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerTests.swift
+//  CupcakeCornerTests
+//
+//  Created by Francisco Manuel Gallegos Luque on 07/02/2025.
+//
+
+import Testing
+@testable import CupcakeCorner
+
+struct CupcakeCornerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
